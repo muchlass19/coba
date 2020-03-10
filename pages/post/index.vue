9a6859c96a4e8603fe2d1da2e461595abcaf7826
MuchlasS <muchlas148@gmail.com>
2020-03-10 17:02:08 +0700

    nambah semua

---
+++ pages/post/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <NavBar></NavBar>
+        <button @click="toggle">klik</button>
+        <h1 v-if="show">Post</h1>
+    </div>
+</template>
+
+<script>
+import NavBar from '~/components/navbar.vue';
+export default {
+    data(){
+        return{
+            show: true,
+        }
+    },
+    
+    components:{
+        NavBar,
+    },
+
+
+    methods:{
+        toggle(){
+            this.show = !this.show
+        }
+    }
+}
+</script>
---
+++ pages/todo/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <NavBar></NavBar>
+        <h1>todo</h1>
+    </div>
+</template>
+
+<script>
+import NavBar from '~/components/navbar.vue'
+export default {
+    components:{
+        NavBar,
+    }
+}
+</script>
